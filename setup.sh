ce62d0e4b20027f853f3415834a9a485c9c5e8c4
James Hall <james.m.f.hall@gmail.com>
2017-07-16 20:24:40 -0400

    Install zsh/oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,21 @@
 
 PRGDIR=$(cd $(dirname $(ls -1 $0)) ; pwd)
 
+zsh_exists=$(which zsh)
+if [ -z "$zsh_exists" ] ; then
+    echo "Installing zsh with sudo"
+    if [ -x "$(which apt-get)" ] ; then
+        sudo apt-get install zsh
+    elif [ -x "$(which yum)" ] ; then
+        sudo yum install zsh
+    fi
+fi
+
+if [ ! -e ~/.oh-my-zsh ] ; then
+    echo "Installing oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
 # general
 ln -sfv ${PRGDIR}/general/profile ~/.profile
 
