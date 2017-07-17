2c633124e04dd7791d0bd0ba3e2f22a91a3df8dc
James Hall <jhall150@bloomberg.net>
2017-07-16 23:27:08 -0400

    fix for work

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ fi
 
 if [ ! -e ~/.oh-my-zsh ] ; then
     echo "Installing oh-my-zsh"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
 
 # general
